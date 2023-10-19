9bfa380a6a62717d66d7253cc6676e883128cadd
SoohwanLee <70312480+Soohwan-Lee@users.noreply.github.com>
2023-10-19 20:07:39 +0900

    <FEAT>: Connect with Google Cloud API

---
+++ dialogFlowPractice.py
@@ -0,0 +1,18 @@
+import os
+import google.cloud.dialogflow_v2 as dialogflow
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'lemme2-hvvh-93b9c4459fd3.json'   #Private Key
+DIALOGFLOW_PROJECT_ID = 'lemme2-hvvh'   #Project ID
+DIALOGFLOW_LANGUAGE_CODE = 'ko'
+our_query = "오늘 날씨 알려줘"
+SESSION_ID = 'me'
+session_client = dialogflow.SessionsClient()
+session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+our_input = dialogflow.types.TextInput(
+    text=our_query, language_code=DIALOGFLOW_LANGUAGE_CODE)
+query = dialogflow.types.QueryInput(text=our_input)
+response = session_client.detect_intent(session=session, query_input=query)
+
+print("Our text:", response.query_result.query_text)
+print("Dialogflow's response:", response.query_result.fulfillment_text)
+print("Dialogflow's intent:", response.query_result.intent.display_name)
