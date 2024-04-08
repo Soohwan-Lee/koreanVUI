b6616da582522052051bee8bd262d538af686d1d
SoohwanLee <70312480+Soohwan-Lee@users.noreply.github.com>
2024-04-08 14:25:35 +0900

    FEAT: Wake up word detection - modulation

---
+++ resource/speechRecognition/wakeUpWordRecognition/wakeUpWordRecognition_module.py
@@ -0,0 +1,44 @@
+import os
+import struct
+import wave
+from pvrecorder import PvRecorder
+import pvporcupine
+
+def wakeUpWordRecognition():
+    # Define hard-coded values here
+    access_key = '9x0FaA466xUKn2jZkaAgAakVVBOG7RV6CFf/jI7rCpHJ3NB0HJ3V1w=='
+    model_path = './resource/speechRecognition/wakeUpWordRecognition/porcupine_params_ko.pv'
+    keyword_paths = ['./resource/speechRecognition/wakeUpWordRecognition/lemmyWakeUp_Mac.ppn']
+    audio_device_index = -1  # Default audio device
+    sensitivities = [0.5] * len(keyword_paths)
+
+    try:
+        porcupine = pvporcupine.create(
+            access_key=access_key,
+            model_path=model_path,
+            keyword_paths=keyword_paths,
+            sensitivities=sensitivities)
+    except pvporcupine.PorcupineError as e:
+        print("Failed to initialize Porcupine: ", e)
+        return
+
+    recorder = PvRecorder(device_index=audio_device_index, frame_length=porcupine.frame_length)
+    recorder.start()
+
+    print('Listening ... (press Ctrl+C to exit)')
+
+    try:
+        while True:
+            pcm = recorder.read()
+            result = porcupine.process(pcm)
+
+            if result >= 0:
+                print("Detected wake-up word!")
+    except KeyboardInterrupt:
+        print('Stopping ...')
+    finally:
+        recorder.delete()
+        porcupine.delete()
+
+if __name__ == '__main__':
+    wakeUpWordRecognition()
